3821fc67082dad78d2f1cdf7baa0b6a47302bd91
= <=>
2017-04-04 22:21:10 -0700

    add static view and capybara tests for elements

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,6 @@
+class StaticController < ApplicationController
+
+  def main
+  end
+
+end
---
+++ test/integration/basic_template_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class BasicTemplateTest < ActionDispatch::IntegrationTest
+  
+  test "it loads a page at the application root" do
+    visit root_path
+    assert_equal 200, page.status_code
+  end
+
+  test "it has an <h1> tag with the content ideabox" do
+    visit root_path
+    assert page.find("h1").has_content? "ideabox"
+  end
+
+  test "it has an ideas container on the page" do
+    visit root_path
+    assert page.has_css? ".idea-container"
+  end
+
+  test "it has a form for creating new ideas" do
+    visit root_path
+    assert page.has_css? "form.new-idea"
+  end
+
+  test "form has an text input for a new idea title" do
+    visit root_path
+    assert page.has_css? "form.new-idea input[type='text'].new-idea-title"
+  end
+
+  test "form has an text input for a new idea body" do
+    visit root_path
+    assert page.has_css? "form.new-idea input[type='text'].new-idea-body"
+  end
+
+  test "form has an input button" do
+    visit root_path
+    assert page.has_css? "form.new-idea input[type='submit'].new-idea-submit"
+  end
+
+end
