39592d3bac56c259dcc0d82d7d38497e389d6c99
= <=>
2017-05-20 22:57:20 -0700

    styling and uniqueness of title

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,5 @@
 class Idea < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :body, presence: true
   validates :quality, inclusion: { in: %w(high medium low) }
 end
